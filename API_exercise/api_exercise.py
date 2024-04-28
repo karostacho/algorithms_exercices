88842acce0ee67f0643590bc37592af50cf7a78f
Karo <karolina.stachowiak0@gmail.com>
2024-04-28 19:49:15 +0200

    Adjust api request

--- API_exercise/api_exercise.py
+++ API_exercise/api_exercise.py
@@ -1,17 +1,16 @@
 from flask import Flask, request, jsonify
-from zadnie_csv import convert_to_json
 app = Flask(__name__)
 app.debug = True
 
 
 @app.route('/process_csv', methods = ['POST'])
 def process_csv():
-	data = convert_to_json()
+	data = request.get_json()
 	try:
 		if not data:
 			return jsonify({'error': 'No data available'}), 400
 		else:
-			return jsonify({'message': 'Data successfully loaded', 'data': data}), 201
+			return jsonify({'message': 'Data successfully loaded','data': data}), 200
 	except:
 		return jsonify({'error':'Internal Server Error'}), 500
 	
