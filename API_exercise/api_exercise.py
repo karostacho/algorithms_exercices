0acce5f92700e90bd6f57de0bf68630d4214075b
Karo <karolina.stachowiak0@gmail.com>
2024-04-28 15:52:10 +0200

    Add API exercise

---
+++ API_exercise/api_exercise.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from zadnie_csv import convert_to_json
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/process_csv', methods = ['POST'])
+def process_csv():
+	data = convert_to_json()
+	try:
+		if not data:
+			return jsonify({'error': 'No data available'}), 400
+		else:
+			return jsonify({'message': 'Data successfully loaded', 'data': data}), 201
+	except:
+		return jsonify({'error':'Internal Server Error'}), 500
+	
+
+if __name__ == "__main__":
+	app.run()
